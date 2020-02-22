42cfc12e76ba13123c2fef1ab38b02b40c235033
Javier <javigdeveloper@gmail.com>
2020-02-22 13:31:13 -0500

    cambio Completo

--- db/migrate/20200222181109_create_events.rb
+++ db/migrate/20200222181109_create_events.rb
@@ -1,6 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.integer :user_id
+      t.integer :availble_places
+      t.integer :sport_id
+      t.string :tipo
     end
   end
 end
