d6c20f5e5fecd58e9c6c7885d44de563e1a765b4
Javier <javigdeveloper@gmail.com>
2020-02-22 13:21:32 -0500

    db cambio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_164653) do
+ActiveRecord::Schema.define(version: 2020_02_22_180325) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 2020_02_22_164653) do
     t.integer "numberOfPlayers"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_sports", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "sport_id"
   end
 
   create_table "users", force: :cascade do |t|
